0bce28ff3ee41b62e71658abc76844bf37c0256e
Shreyas Chaudhari <115451239+shreyaschaudhari04@users.noreply.github.com>
2024-10-17 22:58:55 +0530

    Update middlewares.js

--- API/config/middlewares.js
+++ API/config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Adjust this to your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true, // Enable credentials if you're using cookies or authentication
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
